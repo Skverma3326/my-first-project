778ddbf153d957d49f059a06c5ce9ad213e27724
Skverma3326 <sk1020257366@gmail.com>
2024-09-05 13:04:10 +0530

    footer componets create

---
+++ src/componets/common/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex text-white">
+        <p>logo</p>
+        <p>links</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
