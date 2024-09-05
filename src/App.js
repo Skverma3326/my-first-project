200a7ccad8db2277801297b44c65f1be66616646
Skverma3326 <sk1020257366@gmail.com>
2024-09-05 13:37:01 +0530

    bbbfbf

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './componets/Navbar';
 import Page from './componets/Page';
+import Footer from './componets/common/Footer';
 function App() {
   return (
     <>
     <Navbar/>
     <Page/>
+    <Footer/>
     </>
   );
 }
