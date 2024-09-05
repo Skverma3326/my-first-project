ef2cd66eb4f725688d2e882a5327cacf4b382d7f
dhruvjha-jha <dhruvjha7419179778@gmail.com>
2024-09-05 13:42:42 +0530

    ghjkl

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Navbar from './componets/Navbar';
 import Page from './componets/Page';
 import Footer from './componets/common/Footer';
+import Icons from './componets/common/Icons';
 function App() {
   return (
     <>
     <Navbar/>
     <Page/>
+    <Icons/>
     <Footer/>
     </>
   );
--- src/componets/common/Icons.jsx
+++ src/componets/common/Icons.jsx
@@ -1,1 +1,12 @@
 
+import React from 'react'
+
+const Icons = () => {
+  return (
+    <div>
+      sertyuiop[]
+    </div>
+  )
+}
+
+export default Icons
